bea45e811aa10aa363efa2065fe794cdb9353124
sapnaedutechacademy-cmd <sapnaedutechacademy@gmail.com>
2026-09-09 11:06:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -71,3 +71,21 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+
+
+<script>
+    function openModal(id) {
+        document.getElementById(id).style.display = "block";
+    }
+
+    function closeModal(id) {
+        document.getElementById(id).style.display = "none";
+    }
+
+    // Close modal if user clicks outside the modal box
+    window.onclick = function(event) {
+        if (event.target.classList.contains('custom-modal')) {
+            event.target.style.display = "none";
+        }
+    }
+</script>
